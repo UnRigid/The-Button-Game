58d2c910a840e718db1c6599a517b5bf0d50fdee
Haibayle <unrigidstu@gmail.com>
2025-05-30 19:48:00 +0800

    Add Levels

---
+++ TheButtonGame/Assets/Scripts/Exclusives/Corridor5.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using System.Threading.Tasks;
+using TMPro;
+
+public class Corridor5 : MonoBehaviour
+{
+    
+    public static Corridor5 instance;
+
+    [SerializeField] AudioClip[] audioClip;
+
+    string[] Dialogue = {"dialogue1","dialogue2"};
+
+    static AudioSource audioSource;
+    static GameObject Captions;
+
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(instance);
+        }
+        else
+        {
+            instance = this;
+        }
+
+        audioSource = GameObject.FindGameObjectWithTag("SoundManager").GetComponent<AudioSource>();
+        Captions = GameObject.FindGameObjectWithTag("Captions");
+
+
+        ExitBroadcast.Pressed_Button += PlayPressed;
+
+        PlayInitial();
+    }
+
+    async void PlayInitial()
+    {
+        Captions.GetComponent<TMP_Text>().text = Dialogue[0];
+        Captions.SetActive(true);
+        audioSource.PlayOneShot(audioClip[0], Settings.volume);
+        await Task.Delay((int)(audioClip[0].length * 1000));
+        Captions.SetActive(false);
+        await Task.Yield();
+    }
+
+    async void PlayPressed()
+    {
+        Captions.GetComponent<TMP_Text>().text = Dialogue[1];
+        Captions.SetActive(true);
+        audioSource.PlayOneShot(audioClip[1], Settings.volume);
+        await Task.Delay((int)(audioClip[1].length * 1000));
+        Captions.SetActive(false);
+        Settings.Load();
+        await Task.Yield();
+    }
+
+    private void OnDestroy() {
+        ExitBroadcast.Pressed_Button -= PlayPressed;
+    }
+
+}
